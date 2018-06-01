544f675d5e0024838b3bcfc2e1065e2658de61a1
Christian Vaagan <cvagan@gmail.com>
2018-06-01 14:19:32 +0200

    correcting port

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.post("/imageurl", (req,res) => { entries.handleApiCall(req,res) });
 
 
 //LISTENER
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
 });
